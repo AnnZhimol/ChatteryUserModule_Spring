51beae59276e27d4ae4085a7303ed6bb3815c4f4
AnnZhimol <ankavanka731@gmail.com>
2025-03-19 19:00:41 +0300

    start project

---
+++ src/main/java/com/example/chatteryusermodule/ChatteryUserModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatteryusermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatteryUserModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatteryUserModuleApplication.class, args);
+    }
+
+}
